1440cb48d664bf3aa1d8f4739de48bcdbf5abc5c
Juliano de Medeiros Machado <julianomachado@protonmail.com>
2024-07-03 22:03:08 -0300

    Added health bar node, script and system on the main character script

---
+++ scripts/ProgressBar.gd
@@ -0,0 +1,11 @@
+extends TextureProgressBar
+
+@export var player: Player
+
+func _ready():
+	player.healthChanged.connect(update)
+	update()
+
+func update():
+	value = player.currentHealth * 100 / player.max_health
+
--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -1,8 +1,18 @@
-extends Node2D
+extends CharacterBody2D
+
+class_name Player
+
+signal healthChanged
+
+@export var max_health = 100
+@onready var currentHealth: int = max_health
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	currentHealth -=50 #Testing if the health bar is working by setting half of HP
+	healthChanged.emit() #Emit signal to update health bar
 	pass # Replace with function body.
 
 
