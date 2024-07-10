6528d92affb39af6bf553608573540e3f103a5c1
Juliano de Medeiros Machado <julianomachado@protonmail.com>
2024-07-10 04:45:21 -0300

    Added a villager to rescue in level 1

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 class_name level
 
+@export var villager_counter = 0
 @export var herbs_counter = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
