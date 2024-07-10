7b39d9d83139f012e345e18c833409789f5a9531
Juliano de Medeiros Machado <julianomachado@protonmail.com>
2024-07-09 23:48:16 -0300

    Added paralax layer, temporarily disabled herbs label due to a bug, some alterations on the dummy scene so it can fit the level better

--- scripts/herbs.gd
+++ scripts/herbs.gd
@@ -6,5 +6,5 @@ func _ready():
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.text = str("Herbs Left: ") + str(level.herbs_counter)
-	
+	#self.text = str("Herbs Left: ") + str(level.herbs_counter)
+	pass
