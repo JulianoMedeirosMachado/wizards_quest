ca755c78ea3c68fea2caafb98407595ed573bb9c
Juliano de Medeiros Machado <julianomachado@protonmail.com>
2024-07-10 18:19:42 -0300

    Fixed a wrong script attached to portal

--- scripts/village_portal.gd
+++ scripts/village_portal.gd
@@ -15,7 +15,7 @@ func _on_body_entered(body: PhysicsBody2D):
 func _process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed("accept"):
-			get_tree().change_scene_to_file('res://scenarios/secret_forest.tscn')
+			get_tree().change_scene_to_file('res://scenarios/castle_level.tscn')
 	if (entered == false || entered == true):
 		sprite.animation = 'portal'
 	pass
