ba437754afbc0c27692893ba19e2d250865fa666
Juliano de Medeiros Machado <julianomachado@protonmail.com>
2024-07-10 04:25:40 -0300

    Fixed bug on herbs counter node, which tried to load in different levels

--- scripts/herbs.gd
+++ scripts/herbs.gd
@@ -2,9 +2,16 @@ extends Label
 
 @onready var level = get_tree().get_first_node_in_group("herbs")
 
-func _ready(): 
-	pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	if level and "level_1" in level.get_groups():
+		self.show()
+	else:
+		self.hide()
+
 func _process(delta):
-	#self.text = str("Herbs Left: ") + str(level.herbs_counter)
-	pass
+	if level and "level_1" in level.get_groups():
+		self.text = "Herbs Left: " + str(level.herbs_counter)
+		if level.herbs_counter == 0:
+			self.text = "You made it!!"
+	else:
+		self.hide()
